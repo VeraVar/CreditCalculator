6dedabde0a776caba3adf08f62f02a3c3f75662f
VeraVar <65856530+VeraVar@users.noreply.github.com>
2020-06-25 16:12:29 +0300

    Update CreditPaymentService.java

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,12 +1,9 @@
 public class CreditPaymentService {
-    public long calculate (long creditAmount, long creditPeriodInMonths) {
-            double rateOnYear = 0.0999;
-            double rateOnMonth = rateOnYear / 12;
-
-            double index = rateOnMonth * (Math.pow(1 + rateOnMonth, creditPeriodInMonths) / (Math.pow(1 + rateOnMonth, creditPeriodInMonths) - 1));
-
-            long payment = (long) (creditAmount * index);
-
-            return payment;
-        }
+    public long calculate (long creditAmount, int creditPeriodInMonths) {
+        double rateOnYear = 0.0999;
+        double rateOnMonth = rateOnYear / 12;
+        double index = rateOnMonth * (Math.pow(1 + rateOnMonth, creditPeriodInMonths) / (Math.pow(1 + rateOnMonth, creditPeriodInMonths) - 1));
+        long payment = (long) (creditAmount * index);
+        return payment;
     }
+}
